ca1dcf1512b412345c312f2484bd6c736c6fe772
raccoon <qsx314@gmail.com>
2019-09-03 14:52:23 +0900

    build default tetris

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -12,8 +12,10 @@ export function useInterval(callback, delay) {
     function tick() {
       savedCallback.current();
     }
+
     if (delay !== null) {
       const id = setInterval(tick, delay);
+
       return () => {
         clearInterval(id);
       };
